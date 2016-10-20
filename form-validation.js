c1e351656fcc1420b896ac465849df035a2358d4
Michael Crean <mcrean1@mail.csuchico.edu>
2016-10-19 17:41:12 -0700

    Lab done

--- form-validation.js
+++ form-validation.js
@@ -1,31 +1,48 @@
-function validate(){
+function register()
+{
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  window.alert(passEntered);
+  window.alert(userEntered);
   var txt = userEntered.length;
-  if(txt < 6 || userEntered.indexOf(" ")>=0){
-
+  if(txt < 6 || userEntered.indexOf(" ")>=0)
+  {
   document.getElementById("usernameError").innerHTML
- = "username must be six characters and no spaces.";
-document.getElementById("usernameError").classList.remove("hidden-message");
-document.getElementById("usernameError").classList.add("shown-message");
-document.getElementById("usernameGroup").classList.add("has-error");
-}
+     = "username must be six characters and no spaces.";
+  document.getElementById("usernameError").classList.remove("hidden-message");
+  document.getElementById("usernameError").classList.add("shown-message");
+  document.getElementById("usernameGroup").classList.remove("has-error");
+  }
+
+
 
 var password = "password";
 var lowercasePassword = passEntered.toLowerCase();
-if(lowercasePassword == password){
+
+if(lowercasePassword == password)
+  {
   document.getElementById("passwordError").innerHTML = "password cannot be password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
-  document.getElementById("passwordGroup").classList.add("has-error");
+  document.getElementById("passwordGroup").classList.remove("has-error");
   }
 
-else if(passEntered.length < 6 || passEntered.length > 20) {
+else if(passEntered.length < 6 || passEntered.length > 20)
+  {
   document.getElementById("passwordError").innerHTML = "password must have 6-20 characters."
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
+  }
 }
+
+function validateUsername()
+{
+var userEntered = document.getElementById("user").value;
 }
-var words = "Oonce, Twice, Thrice!";
-var uppercaseWords = words.toUpperCase();
+
+
+function validatePassword()
+{
+  var passEntered = document.getElementById("pass").value;
+}
